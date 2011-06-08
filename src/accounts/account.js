3b6f0b821e2d93e38a36b64043772a19dd41bada
Matt <mattfysh@gmail.com>
2011-06-08 11:18:37 +1000

    balance validation

--- src/accounts/account.js
+++ src/accounts/account.js
@@ -5,6 +5,15 @@ tux.accounts.Account = Backbone.Model.extend({
 	
 	defaults: {
 		balance: 0
+	},
+	
+	adjustBalance: function(amount) {
+		// manually adjust balance to bypass validation
+		this.attributes.balance = this.get('balance') + amount;
+	},
+	
+	validate: function(attrs) {
+		if (attrs.balance) return 'cannot set balance post-creation';
 	}
 	
 });
--- test/accounts/account.spec.js
+++ test/accounts/account.spec.js
@@ -10,6 +10,41 @@ describe('Account model', function() {
 			expect(account.get('balance')).toEqual(0);
 		});
 		
+		it('should allow a balance to be set', function() {
+			var account = new tux.accounts.Account({
+				name: 'test',
+				balance: 100,
+				typeCode: 's'
+			});
+			expect(account.get('balance')).toEqual(100);
+		});
+		
+	});
+	
+	describe('balance adjustments', function() {
+		
+		beforeEach(function() {
+			this.account = new tux.accounts.Account({
+				name: 'test',
+				balance: 10000,
+				typeCode: 's'
+			});
+		});
+		
+		it('should adjust the balance', function() {
+			var account = this.account;
+			account.adjustBalance(200);
+			expect(account.get('balance')).toEqual(10200);
+		});
+		
+		it('should not allow direct updates post-creation', function() {
+			var account = this.account,
+				errorSpy = sinon.spy();
+			account.bind('error', errorSpy);
+			expect(errorSpy.called).toBeTruthy();
+			expect(account.get('balance')).toNotEqual(200);
+		});
+		
 	});
 	
 });
