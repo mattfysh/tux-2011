8e4ffa2ff7ecccfba0c03d4959dcb3d5dc46fa77
Matt <mattfysh@gmail.com>
2011-06-23 17:42:35 +1000

    build on accounts app controller

--- specs/accounts/accountsapp.spec.js
+++ specs/accounts/accountsapp.spec.js
@@ -7,20 +7,43 @@
 	describe('Accounts app', function() {
 		
 		beforeEach(function() {
-			var acc1, acc2, list, view, totalsView;
+			var list, view1, view2, totalsView;
 			
 			// load html fixture
 			jasmine.getFixtures().fixturesPath = '/test/specs/accounts/html';
 			loadFixtures('accounts-app.html');
 			
-			// fake models
-			this.acc1 = acc1 = new Backbone.Model();
-			this.acc2 = acc2 = new Backbone.Model();
+			// fake models and stub collection
+			this.acc1 = new Backbone.Model();
+			this.acc2 = new Backbone.Model();
+			this.list = list = new Backbone.Collection();
+			list.add([this.acc1, this.acc2]);
+			this.listStub = sinon.stub(tux.accounts, 'AccountList')
+				.returns(list);
 			
-			// stub collection
-			list = new Backbone.Collection();
-			list.add([acc1, acc2]);
-			this.listStub = sinon.stub(tux.accounts, 'AccountList').returns(list);
+			// stub view
+			view1 = new Backbone.View({
+				el: $('<tr><td>ANZ</td><td>20</td></tr>')[0]
+			});
+			view2 = new Backbone.View({
+				el: $('<tr><td>CommBank</td><td>300</td></tr>')[0]
+			});
+			this.viewStub = sinon.stub(tux.accounts, 'AccountView');
+			this.viewStub.withArgs({
+				model: this.acc1
+			}).returns(view1);
+			this.viewStub.withArgs({
+				model: this.acc2
+			}).returns(view2);
+			
+			// stub totals view
+			totalsView = new Backbone.View({
+				el: $('<tr><td>Total</td><td>320</td></tr>')[0]
+			});
+			this.totalsStub = sinon.stub(tux.accounts, 'AccountListView');
+			this.totalsStub.withArgs({
+				collection: list
+			}).returns(totalsView);
 			
 			// create app
 			this.accounts = new AccountsApp({
@@ -30,6 +53,8 @@
 		
 		afterEach(function() {
 			this.listStub.restore();
+			this.viewStub.restore();
+			this.totalsStub.restore();
 		});
 		
 		it('should load the accounts list', function() {
@@ -37,15 +62,27 @@
 		});
 		
 		it('should create a new view for each account', function() {
-			
+			expect(this.viewStub.getCall(0)).toHaveBeenCalledWithExactly({
+				model: this.acc1
+			});
+			expect(this.viewStub.getCall(1)).toHaveBeenCalledWithExactly({
+				model: this.acc2
+			});
 		});
 		
 		it('should add each account view in order', function() {
-			
+			expect($('#accounts tr:eq(0) td:eq(0)')).toHaveText('ANZ');
+			expect($('#accounts tr:eq(1) td:eq(0)')).toHaveText('CommBank');
 		});
 		
+		it('should create a totals view for the list', function() {
+			expect(this.totalsStub).toHaveBeenCalledWithExactly({
+				collection: this.list
+			});
+		})
+		
 		it('should add the totals last', function() {
-			
+			expect($('#accounts tr:last td:eq(1)')).toHaveText('320');
 		});
 	
 	});
--- src/accounts/accountsapp.js
+++ src/accounts/accountsapp.js
@@ -7,7 +7,22 @@ namespace('tux.accounts');
 	
 		initialize: function() {
 			// load accounts and render each
-			var list = new tux.accounts.AccountList();
+			var list = new tux.accounts.AccountList(),
+				totals;
+			
+			// render each account
+			list.each(_(function(account) {
+				var view = new tux.accounts.AccountView({
+					model: account
+				});
+				this.$('table').append(view.el);
+			}).bind(this));
+			
+			// render the totals view
+			totals = new tux.accounts.AccountListView({
+				collection: list
+			});
+			this.$('table').append(totals.el);
 		}
 	
 	});
