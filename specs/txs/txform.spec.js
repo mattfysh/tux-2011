2d01bdd53afdbac044df2d6dea8275c2d5336765
Matt <mattfysh@gmail.com>
2011-06-28 17:21:14 +1000

    start txs module

---
+++ specs/txs/txform.spec.js
@@ -0,0 +1,17 @@
+(function() {
+	'use strict';
+	
+	// requires
+	var TxForm = tux.txs.TxForm;
+	
+	describe('Transactions form', function() {
+	
+		loadTemplate('/test/src/txs/jst/tx-form.jst');
+	
+		it('should ', function() {
+			
+		});
+	
+	});
+	
+}());
---
+++ src/txs/txform.js
@@ -0,0 +1,14 @@
+namespace('tux.txs');
+
+(function() {
+	'use strict';
+	
+	tux.txs.TxForm = Backbone.View.extend({
+	
+		initialize: function() {
+			
+		}
+	
+	});
+	
+}());
