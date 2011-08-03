0024760e7945a3c5cbf2e11a42e7f1e9c08a29dd
Matt <mattfysh@gmail.com>
2011-08-03 11:14:22 +1000

    add report model

---
+++ specs/reports/report.spec.js
@@ -0,0 +1,22 @@
+(function() {
+	'use strict';
+	
+	// requires
+	var Report = tux.reports.Report;
+	
+	describe('Report', function() {
+		
+		it('should ', function() {
+
+			//expect().to();
+		})
+		
+		it('should ', function() {
+
+			//expect().to();
+		});
+		
+		
+	});
+	
+}());
---
+++ src/reports/report.js
@@ -0,0 +1,14 @@
+namespace('tux.reports');
+
+(function() {
+	'use strict';
+	
+	tux.reports.Report = Backbone.Model.extend({
+	
+		initialize: function() {
+			
+		}
+	
+	});
+	
+}());
